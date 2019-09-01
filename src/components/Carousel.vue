40c3716e561d04e0ccb9e8580f4a12169054fabc
Roberta <robertaf898@gmail.com>
2019-09-01 20:14:17 +0100

    edits carousel in main style and adjusts media queries for mobile

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,11 +1,13 @@
 <template>
   <div id="carousel">
-    <img :src="arrows.arrowLeft" class="arrows" @click="gotToPreviousSlide"/>
+    <!-- <img :src="arrows.arrowLeft" class="arrows" @click="gotToPreviousSlide"/> -->
+  <div class="carousel-control-prev-icon" @click="gotToPreviousSlide"></div>
     <div id="petBox">
         <h3 id="petName">Meet {{petName}}</h3>
          <img :src="this.singleSlide" width="500px" height="500px" id="image"/>
     </div>
-    <img :src="arrows.arrowRight" class="arrows" @click="goToNextSlide"/>
+    <!-- <img :src="arrows.arrowRight" class="arrows" @click="goToNextSlide"/> -->
+    <div class="carousel-control-next-icon" @click="goToNextSlide"></div>
   </div>
 </template>
 
